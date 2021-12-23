52e6558ab3492b95c422633af6f5448538d779e4
onhacker <onh4cker@gmail.com>
2021-12-23 21:46:36 +0800

    ads

--- application/modules/admin_sirkulasi/models/M_admin_sirkulasi.php
+++ application/modules/admin_sirkulasi/models/M_admin_sirkulasi.php
@@ -9,7 +9,7 @@ class M_admin_sirkulasi extends CI_Model {
 		parent::__construct();
 	}
 	private function get_data_query(){
-		$this->db->where("status","0");
+		// $this->db->where("status","1");
 		$this->db->from($this->table);
 
 		$i = 0;
