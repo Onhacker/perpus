090d813868313825b62196602179bded5a110cc8
onhacker <onh4cker@gmail.com>
2021-12-25 14:07:09 +0800

    as

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -18,6 +18,30 @@ class Home extends Onhacker_Controller {
         
 	}
 
+	function wa(){
+		$telepon = "085288886853";
+		$message = "tes kirim wa";
+		$curl = curl_init();
+                $data = [
+                    'phone' => "$telepon",
+                    'message' => "$message",
+                ];
+
+                curl_setopt($curl, CURLOPT_HTTPHEADER,
+                    array(
+                        "Authorization: $token",
+                    )
+                );
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+                curl_setopt($curl, CURLOPT_URL, config('https://kacangan.wablas.com')."/3PT7MarvSGrLFYBWfDefa5rQCagjpWsBhjchS6etlp0n6FLU9oAPKOGpNawOzeO1/send-message");
+                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+                $result = curl_exec($curl);
+                curl_close($curl);
+	}
+
 	function timezone(){
         $this->db->where("id_identitas", "1");
         $s = $this->db->get("identitas")->row();
