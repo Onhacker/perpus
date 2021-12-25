b7dfad0a06957ef2922c2b17a0bf66f68c579382
onhacker <onh4cker@gmail.com>
2021-12-25 14:12:17 +0800

    as

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -41,7 +41,8 @@ class Home extends Onhacker_Controller {
                 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
                 $result = curl_exec($curl);
                 curl_close($curl);
-                echo "sending";
+                echo "<pre>";
+print_r($result);
 	}
 
 	function timezone(){
